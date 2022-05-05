107671b1d0516cd5f10be4563b08b144227a65d0
blakeyi <1152420971@qq.com>
2022-05-05 23:37:48 +0800

    chore:增加格式化代码

--- code/cpp/leetcodeClassify.cpp
+++ code/cpp/leetcodeClassify.cpp
@@ -3,33 +3,57 @@
 ** 题目链接:https://leetcode-cn.com/problems/climbing-stairs/?utm_source=LCUS&utm_medium=ip_redirect&utm_campaign=transfer2china
 ** 题目简述:爬楼梯, 一次只能怕一阶或者两阶
 ** 题目分类:动态规划
-** 解题思路:状态转移方程, dp[i] = i, i <= 2; dp[i] = dp[i - 1] + dp[i - 2], n > 2
+** 解题思路:状态转移方程, dp[i] = i, i <= 2; dp[i] = dp[i - 1] + dp[i - 2], n >
+*2
 ** 反思收获:注意边界条件处理
 */
 
-class Solution70 {
-public:
+class Solution70
+{
+  public:
     int climbStairs(int n) {
         if (n <= 2) {
             return n;
         }
-        vector<int> dp(n + 1, 1); // 初始化为1,可以取消dp单独赋值,然后i从2开始
+        vector<int> dp(n + 1, 1);  // 初始化为1,可以取消dp单独赋值,然后i从2开始
         for (int i = 2; i <= n; ++i) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
     }
-     // 优化空间复杂度,dp[i]只与前两个相关
+    // 优化空间复杂度,dp[i]只与前两个相关
     int climbStairs1(int n) {
         if (n <= 2) {
             return n;
         }
         int pre1 = 1, pre2 = 2, cur;
         for (int i = 3; i <= n; ++i) {
-            cur = pre1 + pre2;
+            cur  = pre1 + pre2;
             pre1 = pre2;
             pre2 = cur;
         }
         return cur;
+    }
+};
+
+/*
+** 题目链接:https://leetcode-cn.com/problems/house-robber/?utm_source=LCUS&utm_medium=ip_redirect&utm_campaign=transfer2china
+** 题目简述:打家劫舍
+** 题目分类:动态规划
+** 解题思路: dp[i] = max(dp[i-1], dp[i-2] + nums[i-1]), 取决于当前的这个拿不拿
+** 反思收获:
+*/
+
+class Solution198
+{
+  public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n + 1, 0);
+        dp[1] = nums[0];
+        for (size_t i = 2; i <= n; i++) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
+        return dp[n];
     }
 };
--- code/cpp/main.cpp
+++ code/cpp/main.cpp
@@ -2,37 +2,24 @@
 using namespace std;
 
 /*
-** 题目链接:https://leetcode-cn.com/problems/climbing-stairs/?utm_source=LCUS&utm_medium=ip_redirect&utm_campaign=transfer2china
-** 题目简述:爬楼梯
+** 题目链接:https://leetcode-cn.com/problems/house-robber/?utm_source=LCUS&utm_medium=ip_redirect&utm_campaign=transfer2china
+** 题目简述:打家劫舍
 ** 题目分类:动态规划
-** 解题思路:
+** 解题思路: dp[i] = max(dp[i-1], dp[i-2] + nums[i-1]), 取决于当前的这个拿不拿
 ** 反思收获:
 */
 
-class Solution70 {
-public:
-    int climbStairs(int n) {
-        if (n <= 2) {
-            return n;
-        }
-        vector<int> dp(n + 1, 1); // 初始化为1,可以取消dp单独赋值,然后i从2开始
-        for (int i = 2; i <= n; ++i) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+class Solution198
+{
+  public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n + 1, 0);
+        dp[1] = nums[0];
+        for (size_t i = 2; i <= n; i++) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1]);
         }
         return dp[n];
-    }
-    // 优化空间复杂度,dp[i]只与前两个相关
-    int climbStairs1(int n) {
-        if (n <= 2) {
-            return n;
-        }
-        int pre1 = 1, pre2 = 2, cur;
-        for (int i = 3; i <= n; ++i) {
-            cur = pre1 + pre2;
-            pre1 = pre2;
-            pre2 = cur;
-        }
-        return cur;
     }
 };
 
