4f18bfc17ff1a7044a3b9c952f4e8aab32ddd545
blakeyi <blakeyi@tencent.com>
2022-05-10 10:46:48 +0800

    chore:printf bool失败

--- code/cpp/leetcodeClassify.cpp
+++ code/cpp/leetcodeClassify.cpp
@@ -1,4 +1,7 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
 /*
 ** 题目链接:https://leetcode-cn.com/problems/climbing-stairs/?utm_source=LCUS&utm_medium=ip_redirect&utm_campaign=transfer2china
 ** 题目简述:爬楼梯, 一次只能怕一阶或者两阶
@@ -28,7 +31,7 @@ class Solution70
         }
         int pre1 = 1, pre2 = 2, cur;
         for (int i = 3; i <= n; ++i) {
-            cur  = pre1 + pre2;
+            cur = pre1 + pre2;
             pre1 = pre2;
             pre2 = cur;
         }
@@ -55,5 +58,57 @@ class Solution198
             dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1]);
         }
         return dp[n];
+    }
+};
+
+/*
+**
+题目链接:https://leetcode-cn.com/problems/longest-increasing-subsequence/?utm_source=LCUS&utm_medium=ip_redirect&utm_campaign=transfer2china
+** 题目简述:最长递增子序列
+** 题目分类:动态规划
+** 解题思路:
+解法1: 由于不是连续的,对于当前的nums[i],
+需要向前遍历[0,i-1]的所有元素,如果nums[i] > nums[j],
+则比较dp[i]和dp[j]+1的大小,至于为什么要比较,是因为dp[i]可能会一直变
+解法2:解法1的时间间复杂度为O(n^2),dp来动态更新最长的序列,如果是大于dp中最大的,则直接插入队尾,如果不是则利用low_bound找到合适的位置插入
+
+** 反思收获:
+*/
+
+class Solution300
+{
+  public:
+    int lengthOfLIS(vector<int>& nums) {
+        int maxLen = 0, n = nums.size();
+        if (n <= 1) {
+            return n;
+        }
+        vector<int> dp(n, 1);
+        for (size_t i = 1; i < n; i++) {
+            for (size_t j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLen = max(maxLen, dp[i]);
+        }
+        return maxLen;
+    }
+    int lengthOfLIS1(vector<int>& nums) {
+        int maxLen = 0, n = nums.size();
+        if (n <= 1) {
+            return n;
+        }
+        vector<int> dp;
+        dp.push_back(nums[0]);
+        for (size_t i = 1; i < n; i++) {
+            if (dp.back() < nums[i]) {
+                dp.push_back(nums[i]);
+            } else {
+                auto iter = lower_bound(dp.begin(), dp.end(), nums[i]);
+                *iter = nums[i];
+            }
+        }
+        return dp.size();
     }
 };
--- code/cpp/main.cpp
+++ code/cpp/main.cpp
@@ -2,60 +2,42 @@
 using namespace std;
 
 /*
-**
-题目链接:https://leetcode-cn.com/problems/longest-increasing-subsequence/?utm_source=LCUS&utm_medium=ip_redirect&utm_campaign=transfer2china
-** 题目简述:最长递增子序列
-** 题目分类:动态规划
-** 解题思路:
-解法1: 由于不是连续的,对于当前的nums[i],
-需要向前遍历[0,i-1]的所有元素,如果nums[i] > nums[j],
-则比较dp[i]和dp[j]+1的大小,至于为什么要比较,是因为dp[i]可能会一直变
-解法2:解法1的时间间复杂度为O(n^2),dp来动态更新最长的序列,如果是大于dp中最大的,则直接插入队尾,如果不是则利用low_bound找到合适的位置插入
-
+** 题目链接:https://leetcode.cn/problems/partition-equal-subset-sum/
+** 题目简述:分割等和子集
+** 题目分类:动态规划(01背包问题)
+** 解题思路:就是从n个数里找m个数使得其和为总和的一半
+用二维bool数组,dp[i][j]表示,从遍历到第i个数时,其和为j的可能性是否为真,因此dp[i][0]都为true,因为可以全不选,就为0
 ** 反思收获:
 */
 
-class Solution300
+class Solution416
 {
   public:
-    int lengthOfLIS(vector<int>& nums) {
-        int maxLen = 0, n = nums.size();
-        if (n <= 1) {
-            return n;
+    bool canPartition(vector<int>& nums) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum % 2)
+            return false;
+        int target = sum / 2, n = nums.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
+        for (int i = 0; i <= n; ++i) {
+            dp[i][0] = true;
         }
-        vector<int> dp(n, 1);
-        for (size_t i = 1; i < n; i++) {
-            for (size_t j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = max(dp[i], dp[j] + 1);
-                }
-            }
-            maxLen = max(maxLen, dp[i]);
-        }
-        return maxLen;
-    }
-    int lengthOfLIS1(vector<int>& nums) {
-        int maxLen = 0, n = nums.size();
-        if (n <= 1) {
-            return n;
-        }
-        vector<int> dp;
-        dp.push_back(nums[0]);
-        for (size_t i = 1; i < n; i++) {
-            if (dp.back() < nums[i]) {
-                dp.push_back(nums[i]);
-            } else {
-                auto iter = lower_bound(dp.begin(), dp.end(), nums[i]);
-                *iter = nums[i];
+        for (int i = 1; i <= n; ++i) {
+            for (int j = nums[i - 1]; j <= target; ++j) {
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                printf("dp[%d][%d] = %d\n", i, j, dp[i][j]);
             }
         }
-        return dp.size();
+        return dp[n][target];
     }
 };
 
 int main() {
-    vector<int> nums = {1, 3, 4, 7};
-    auto iter = lower_bound(nums.begin(), nums.end(), 2, greater<int>());
-    cout << *iter;
+    Solution416 so;
+    vector<int> nums = {1, 5, 10, 6};
+    bool a = true;
+    printf("%d", a);
+    so.canPartition(nums);
+
     return 0;
 }
