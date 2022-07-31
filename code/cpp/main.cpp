2b41d79d28c8d9831bf8803d4dbb67d14e48b753
blakeyi <1152420971@qq.com>
2022-07-31 11:48:19 +0800

    chore:dfs

--- code/cpp/main.cpp
+++ code/cpp/main.cpp
@@ -2,47 +2,58 @@
 using namespace std;
 
 /*
-** 题目链接:https://leetcode.cn/problems/flatten-binary-tree-to-linked-list/
-** 题目简述:114. 二叉树展开为链表
-** 题目分类:链表和二叉树
+** 题目链接:https://leetcode-cn.com/problems/max-area-of-island/
+** 题目简述:695. 岛屿的最大面积
+** 题目分类:深度优先搜索
 ** 反思收获:
+** 三种写法:
+** 1.用栈模拟递归,首先遍历二维数组,
+*对于值为1的点开始搜索,用栈存储,把当前点的上下左右四个方向上的点遍历一遍,如果为1,则入栈,遍历完后再从栈顶取元素重新开始,直到栈中无元素结束
 */
 
-class Solution114
+class Solution695
 {
   public:
-    void flatten(TreeNode* root) {
-        vector<TreeNode*> list;
-        preOrder(root, list);
-        if (list.size() <= 1) {
-            return;
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = m ? grid[0].size() : 0;
+        int maxSize = 0;
+        vector<int> direction{-1, 0, 1, 0, -1};
+        for (size_t i = 0; i < m; i++) {
+            for (size_t j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    grid[i][j] = 0;  // 防止重复搜索
+                    stack<pair<int, int>> island;
+                    island.push({i, j});
+                    int areaSize = 1;
+                    while (!island.empty()) {
+                        auto [l, r] = island.top();
+                        island.pop();
+                        for (size_t k = 0; k < 4; k++) {
+                            int x = l + direction[k];
+                            int y = r + direction[k + 1];
+                            if (x >= 0 && x < m && y >= 0 && y < n &&
+                                grid[x][y]) {
+                                areaSize++;
+                                grid[x][y] = 0;  // 重点
+                                island.push({x, y});
+                            }
+                        }
+                    }
+                    maxSize = max(maxSize, areaSize);
+                }
+            }
         }
-        for (size_t i = 0; i < list.size() - 1; i++) {
-            list[i]->left = nullptr;
-            list[i]->right = list[i + 1];
-        }
-        return;
-    }
-    void preOrder(TreeNode* root, vector<TreeNode*>& list) {
-        if (root == nullptr) {
-            return;
-        }
-        list.push_back(root);
-        preOrder(root->left, list);
-        preOrder(root->right, list);
+        return maxSize;
     }
 };
 
 int main() {
-    Solution114 so;
-    vector<int> input = {1, 3, 5, INT_MAX, INT_MAX, INT_MAX, INT_MAX};
+    Solution695 so;
+    vector<vector<int>> input = {{1, 0, 1, 1, 0, 1, 0, 1},
+                                 {1, 0, 1, 1, 0, 1, 1, 1},
+                                 {0, 0, 0, 0, 0, 0, 0, 1}};
 
-    // vector<vector<int>> ans = {{1}, {1, 3, 5}, {5}};
-    // for (auto i : ans) {
-    //     cout << i.size();
-    // }
-
-    TreeNode* root = CreatTree(input);
-    so.flatten(root);
+    so.maxAreaOfIsland(input);
     return 0;
 }
